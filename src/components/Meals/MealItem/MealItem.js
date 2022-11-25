2b6eb8762f526403b67fda83df2fdb2b88439838
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-25 10:35:35 +0300

    create MealItemForm

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,7 @@
 import styles from "./MealItem.module.css";
 
+import MealItemForm from "./MealItemForm";
+
 function MealItem(props) {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -10,7 +12,9 @@ function MealItem(props) {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import styles from "./MealItemForm.module.css";
+
+function MealItemForm(props) {
+  return (
+    <form className={styles.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+}
+
+export default MealItemForm;
