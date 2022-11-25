e6594ef8c6da36091667c3ec701b528c208b8315
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-25 10:43:53 +0300

    add Input component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,21 @@
 import styles from "./MealItemForm.module.css";
 
+import Input from "../../UI/Input";
+
 function MealItemForm(props) {
   return (
     <form className={styles.form}>
-      <input />
+      <Input
+        label="Quantity"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
