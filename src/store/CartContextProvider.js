6668090bdeae506feb7a34a4cd6aa146a16e7ae4
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-25 14:44:53 +0300

    manage adding items to cart

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,13 +1,37 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = function (state, action) {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+
+  return defaultCartState;
+};
 
 const CartContextProvider = function (props) {
-  const addItemsToCartHandler = function (item) {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemsToCartHandler = function (item) {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
   const removeItemsFromCartHandler = function (id) {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemsToCartHandler,
     removeItem: removeItemsFromCartHandler,
   };
