f44b90118c2c0054837451b9a9c0e8a493693312
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-26 22:10:16 +0300

    remove dummy data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,33 +3,6 @@ import MealItem from "./MealItem/MealItem";
 import { useEffect, useState } from "react";
 import styles from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 function AvailableMeals(props) {
   const [mealsList, setMealsList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
