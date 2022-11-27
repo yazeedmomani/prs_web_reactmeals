83089c3560438538f3fa06a06efc212b0bc53cac
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-27 03:52:25 +0300

    bug fix

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,5 +1,5 @@
 import styles from "./Checkout.module.css";
-import { useRef, useReducer, useState } from "react";
+import { useRef, useReducer, useState, useEffect } from "react";
 import { getByDisplayValue } from "@testing-library/react";
 
 const isEmpty = (item) => item.value.trim().length === 0;
@@ -26,8 +26,16 @@ const isValidReducer = function (state, action) {
       if (key === "type") continue;
       updated[key] = val;
     }
-    console.log(updated);
     return updated;
+  }
+
+  if (action.type === "RESET") {
+    return {
+      name: true,
+      street: true,
+      postal: true,
+      city: true,
+    };
   }
 
   return isValidInitials;
@@ -35,12 +43,20 @@ const isValidReducer = function (state, action) {
 
 const Checkout = (props) => {
   // Changing it's state makes react rerender this component
-  const [render, setRender] = useState(false);
+  const [_, setRender] = useState(false);
   const [isValid, dispatch] = useReducer(isValidReducer, isValidInitials);
   const nameRef = useRef();
   const streetRef = useRef();
   const postalRef = useRef();
   const cityRef = useRef();
+
+  //Reset
+  useEffect(() => {
+    dispatch({
+      type: "RESET",
+    });
+    setRender((prev) => !prev);
+  }, []);
 
   // Validate on submit
   const confirmHandler = (event) => {
@@ -75,8 +91,6 @@ const Checkout = (props) => {
     setRender((prev) => !prev);
     return;
   };
-
-  console.log(isValid.name || "invalid");
 
   return (
     <form
